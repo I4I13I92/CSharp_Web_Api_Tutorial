b52c30623247f0a7fb9a619fb757316ce55f46a9
Lavic13 <vmoo1392@gmail.com>
2019-09-10 10:40:36 -0700

    complete js/ajax later

--- wwwroot/site.js
+++ wwwroot/site.js
@@ -1,1 +1,37 @@
-﻿
+﻿const uir = "todo/api";
+let todos = null;
+function getCount(data)
+{
+    const el = $("#counter");
+    let name = "to-do";
+    if (data)
+    {
+        if (data > 1) {
+            name = "to-dos";
+        }
+        el.text(data + " " + name)
+    }
+    else
+    {
+        el.text("No" + name);
+    }
+}
+
+$(document).ready(function () {
+    getData();
+})
+
+function getData() {
+    $.ajax({
+        type: "Get",
+        url: uri,
+        cache: false,
+        success: function (data) {
+            const tBody = $("#todos");
+
+            $(tBody).empty();
+            getCount(data.length);
+
+        }
+    })
+}
